c471a05fc754c360753c2702a89c53e15e6ef03d
Yoshi <yoshi@Yoshis-MacBook-Pro.local>
2016-12-30 16:16:33 +0900

    [branch_name]

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-docker run -p 4444:4444 --name yoshi-yesod_api_server -it yoshidocker_yesod_api_server bash
-
+docker run -p 4444:4444 --name yoshidocker_yesod_api_server -it yoshidocker_yesod_api_server bash
 
